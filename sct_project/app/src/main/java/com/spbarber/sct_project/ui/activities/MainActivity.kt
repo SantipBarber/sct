27e5cecc020c08c0eb85ade6dcc1110b9c1799db
Santiago Pérez Barber <santipbr@gmail.com>
2021-04-19 23:08:17 +0200

    Experiencia Fragment Realizado

--- sct_project/app/src/main/java/com/spbarber/sct_project/ui/activities/MainActivity.kt
+++ sct_project/app/src/main/java/com/spbarber/sct_project/ui/activities/MainActivity.kt
@@ -1,11 +1,9 @@
-package com.spbarber.sct_project.ui
+package com.spbarber.sct_project.ui.activities
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Scroller
 import com.google.android.material.snackbar.Snackbar
-import com.spbarber.sct_project.databinding.ActivityLoginBinding
 import com.spbarber.sct_project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +15,5 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        binding.btnToLogin.setOnClickListener {
-            val intent = Intent(this, login::class.java)
-            Snackbar.make(view, "Vamos hacia el Login", Snackbar.LENGTH_LONG).show()
-            startActivity(intent)
-        }
     }
 }
--- sct_project/app/src/main/java/com/spbarber/sct_project/ui/fragments/InitFragment.kt
+++ sct_project/app/src/main/java/com/spbarber/sct_project/ui/fragments/InitFragment.kt
@@ -1,33 +1,29 @@
 package com.spbarber.sct_project.ui.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.material.snackbar.Snackbar
+import androidx.navigation.fragment.NavHostFragment
 import com.spbarber.sct_project.R
-import com.spbarber.sct_project.databinding.FragmentLogoInfoBinding
-import com.spbarber.sct_project.ui.activities.login
+import com.spbarber.sct_project.databinding.FragmentInitBinding
 
 
-class LogoInfoFragment : Fragment() {
-    private lateinit var binding: FragmentLogoInfoBinding
+class InitFragment : Fragment() {
+    private lateinit var binding: FragmentInitBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        binding = FragmentLogoInfoBinding.inflate(layoutInflater)
+        binding = FragmentInitBinding.inflate(layoutInflater)
         val view = binding.root
 
         binding.btnToLogin.setOnClickListener {
-            val intent = Intent(activity, login::class.java)
-            Snackbar.make(view, "Vamos hacia el Login", Snackbar.LENGTH_LONG).show()
-            startActivity(intent)
+            NavHostFragment.findNavController(this).navigate(R.id.action_initFragment_to_loginFragment)
         }
 
         binding.btnStartSignin.setOnClickListener {
-
+            NavHostFragment.findNavController(this).navigate(R.id.action_initFragment_to_experienceFragment)
         }
 
         return view
